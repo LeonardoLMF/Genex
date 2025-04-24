f817ff17988d922b6e0a4a2565337685a20da62d
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-04-24 14:00:44 -0300

    first commit

---
+++ src/main/java/com/geraleo/genex/GenexApplication.java
@@ -0,0 +1,13 @@
+package com.geraleo.genex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geraleo/genex/GenexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraleo.genex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
