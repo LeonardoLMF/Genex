9e24a1acb120a08fed8060567319f10e2c36a0fa
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-04-28 11:34:37 -0300

    improvement: melhoria no CadastroQuestaoDTO

--- src/main/java/com/geraleo/genex/dto/CadastroQuestaoDTO.java
+++ src/main/java/com/geraleo/genex/dto/CadastroQuestaoDTO.java
@@ -1,14 +1,27 @@
 package com.geraleo.genex.dto;
 
+import com.geraleo.genex.domain.NivelDificuldade;
+import com.geraleo.genex.domain.TipoQuestao;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CadastroQuestaoDTO {
+    @NotBlank(message = "Campo obrigatório.")
     private String enunciado;
+
+    @NotBlank(message = "A respostá da questão é obrigatória.")
     private String resposta;
 
-    public String getEnunciado(){
-        return enunciado;
-    }
+    @NotBlank(message = "O tópico da questão é obrigatorio.")
+    private String topico;
 
-    public String getResposta(){
-        return resposta;
-    }
+    @NotNull(message = "O nivel da questão é obrigatorio.")
+    private NivelDificuldade dificuldade;
+
+    @NotNull(message = "O tipo da questão é obrigatorio.")
+    private TipoQuestao tipo;
 }
