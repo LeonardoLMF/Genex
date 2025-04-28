723f7d727163c30651def43ee257608dd2b560e3
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-04-28 10:01:10 -0300

    feat: criação do dto CadastroQuestaoDTO

---
+++ src/main/java/com/geraleo/genex/dto/CadastroQuestaoDTO.java
@@ -0,0 +1,14 @@
+package com.geraleo.genex.dto;
+
+public class CadastroQuestaoDTO {
+    private String enunciado;
+    private String resposta;
+
+    public String getEnunciado(){
+        return enunciado;
+    }
+
+    public String getResposta(){
+        return resposta;
+    }
+}
