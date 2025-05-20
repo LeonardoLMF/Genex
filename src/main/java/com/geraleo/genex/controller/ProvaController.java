3cedbaa66b9a38c4bdc2230c66900fab333b0db6
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-05-20 11:15:49 -0300

    feat: implementar a visualização HTML de provas com o thymeleaf

--- src/main/java/com/geraleo/genex/controller/ProvaController.java
+++ src/main/java/com/geraleo/genex/controller/ProvaController.java
@@ -1,19 +1,20 @@
 package com.geraleo.genex.controller;
 
+import com.geraleo.genex.domain.NivelDificuldade;
+import com.geraleo.genex.domain.TipoQuestao;
 import com.geraleo.genex.dto.GerarProvaDTO;
 import com.geraleo.genex.dto.ProvaGeradaDTO;
 import com.geraleo.genex.service.ProvaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/provas")
 @RequiredArgsConstructor
 public class ProvaController {
@@ -25,6 +26,27 @@ public class ProvaController {
         List<ProvaGeradaDTO> prova = provaService.gerarProva(dto);
         return ResponseEntity.ok(prova);
     }
+
+    @GetMapping("/visualizar")
+    public String visualizarProva(@RequestParam String topico,
+                                  @RequestParam NivelDificuldade dificuldade,
+                                  @RequestParam TipoQuestao tipo,
+                                  @RequestParam int quantidade,
+                                  Model model) {
+
+        GerarProvaDTO dto = new GerarProvaDTO();
+        dto.setTopico(topico);
+        dto.setDificuldade(dificuldade);
+        dto.setTipo(tipo);
+        dto.setQuantidade(quantidade);
+
+        List<ProvaGeradaDTO> questoes = provaService.gerarProva(dto);
+
+        model.addAttribute("questoes", questoes);
+        return "prova";
+    }
+
+
 
 
 }
